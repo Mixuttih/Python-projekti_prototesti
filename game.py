cad8d31e55c56695b345b070fd46768bf5558243
Mixuttih <alanhommis@gmail.com>
2025-09-15 11:50:13 +0300

    asd

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import random
 
 import mysql.connector
 
-conn = mysql.connector.connect(
+yhteys = mysql.connector.connect(
     host='localhost',
     port=3307,
     database='flight_game',
@@ -10,5 +10,32 @@ conn = mysql.connector.connect(
     password='mikasana',
     autocommit=True
 )
+
+
+def kysymys():
+    sql = f"SELECT name FROM airport ORDER BY RAND() LIMIT 1"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchone()
+    for rivi in tulos:
+        return rivi
+    return None
+
+def oikea_vastaus(i):
+    sql = f"SELECT iso_country FROM airport WHERE name = {i}"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchone()
+    for rivi in tulos:
+        return rivi
+    return None
+
+
 print("Welcome to 'Who Wants to be a Millionaire?' Airport Edition!")
 username = input('Enter your username: ')
+
+print(f"Alright, {username}! Your first question is...")
+question = kysymys()
+answer = oikea_vastaus(question)
+print(f"Which country is {question} located in?")
+print(f"{answer}")
